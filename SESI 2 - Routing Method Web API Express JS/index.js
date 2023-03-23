20de9960f250578603104214669b4f22dcc38528
Your Name <you@example.com>
2023-03-23 15:41:38 +0700

    selesai

--- SESI 2 - Routing Method Web API Express JS/index.js
+++ SESI 2 - Routing Method Web API Express JS/index.js
@@ -4,17 +4,20 @@ const port=3000;
 const bodyParser = require("body-parser")
 
 
+
+
 app.use(bodyParser.json())
 
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
-
+//post data bisa menggunakan (req.query / req.body)
 app.post("/login",(req,res)=>{
     console.log(req.query.Kelas)
     res.send("Login Berhasil")
 })
 
+// untuk put data
 app.put("/unsername",(req,res)=>{
     console.log(req.body)
     res.send("Update Berhasil")
